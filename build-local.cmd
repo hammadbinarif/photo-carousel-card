1b5fea28bcf2d64a1cf7d419bdbe76c898613bc8
Hammad Arif <hammadbinarif@gmail.com>
2025-05-30 21:12:59 +1000

    dist folder created

--- build-local.cmd
+++ build-local.cmd
@@ -1,1 +1,1 @@
-npm run build && scp -P 2222 ./photo-carousel-card.js root@192.168.4.101:/config/www/photo-carousel-card/photo-carousel-card.js
+npm run build && scp -P 2222 ./dist/photo-carousel-card.js root@192.168.4.101:/config/www/photo-carousel-card/photo-carousel-card.js
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // Change to 'production' for production builds
+  mode: 'production', 
   entry: './src/photo-carousel-card.js',
   output: {
     filename: 'photo-carousel-card.js',
-    path: __dirname,
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -26,18 +26,17 @@ module.exports = {
     extensions: ['.js', '.json'],
     modules: ['node_modules'],
   },
-  // optimization: {
-  //   minimizer: [
-  //     new TerserPlugin({
-  //       terserOptions: {
-  //         output: {
-  //           comments: false, // Remove all comments, including license comments
-  //         },
-  //       },
-  //       extractComments: false, // Prevent extracting license comments into a separate file
-  //     }),
-  //   ],
-  // },
-    devtool: 'eval-source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false, // Remove all comments, including license comments
+          },
+        },
+        extractComments: false, // Prevent extracting license comments into a separate file
+      }),
+    ],
+  }
 
 };
